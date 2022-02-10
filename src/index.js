88ac31ff2e53d0b7b49de8aba08ceeec4e32fd8e
JayMartMedia <Jay@JayMartMedia.com>
2022-02-10 16:14:00 -0500

    Init project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// setup express
+const app = express();
+const port = 3000;
+
+// setup routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// start express
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+});
