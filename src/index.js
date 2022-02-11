82248afd2085c225173cf80e4fde84c9c644927d
JayMartMedia <Jay@JayMartMedia.com>
2022-02-11 13:07:42 -0500

    Fixed standard.js linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import express from 'express';
+import express from 'express'
 
 // setup express
-const app = express();
-const port = 3000;
-
-const myUnusedVariable = 2;
+const app = express()
+const port = 3000
 
 // setup routes
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+  res.send('Hello, world!')
+})
 
 // start express
 app.listen(port, () => {
-    console.log(`App listening on port: ${port}`);
-});
+  console.log(`App listening on port: ${port}`)
+})
